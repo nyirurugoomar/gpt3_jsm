44e8b00017fe127a041b3c5a23c5b8970bf4eb14
nyirurugo omar <nyirurugoomar@gmail.com>
2022-05-30 08:53:04 +0200

    Navbar logo and menu

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { RiMenu3Line,RiCloseLin } from 'react-icons/ri'
 import './navbar.css';
-
+import logo from '../../assets/logo.svg'
 
 const navbar = () => {
   return (
     <div className='gpt3__navbar'>
       <div className='gpt3__navbar-links'>
-
+         <div className='gpt3__navbar-link_logo'>
+           <img src={logo} alt= "logo" />
+         </div>
+         <div className='gpt3__navbar-links_container'>
+           <p><a href='#home'>Home</a></p>
+           <p><a href='#wgpt3'>What is GPT3?</a></p>
+           <p><a href='#possibility'>Open AI</a></p>
+           <p><a href='#ffeatures'>Case Studies</a></p>
+           <p><a href='#blog'>Library</a></p>
+         </div>
       </div>
     </div>
   )
