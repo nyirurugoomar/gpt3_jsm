523a81e0fc4b13fd6a4ddcb6349dbe40d760f829
nyirurugo omar <nyirurugoomar@gmail.com>
2022-06-01 22:06:43 +0200

    what is gpt3 page and its css

--- src/containers/whatGPT3/whatGPT3.jsx
+++ src/containers/whatGPT3/whatGPT3.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Feature } from '../../components'
 import './whatGPT3.css'
 
 const WhatGPT3 = () => {
   return (
-    <div>
-        WhatGPT3
+    <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
+         
+         <div className='gpt3__whatgpt3-feature'>
+           <Feature />
+         </div>
+         <div className='gpt3__whatgpt3-heading'>
+           <h1 className='gradient-text'>
+           The possibilities are beyond your imagination
+           </h1>
+         </div>
     </div>
   )
 }
